278fd2762b3239b6110de7f6f6441f27a9dc1d3c
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-03-16 22:58:36 +0300

    Use two different databases for test
    Also adds `noProgress` to clone command and fixes issue
    with `co(...).finally` not being a function, as `co()` seem
    to return native Promise object, rather than Bluebird.

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,5 @@
 const assert = require('assert')
 const co = require('co')
-const r = require('rethinkdbdash')({silent: true})
 const clone = require('../lib/commands/clone')
 const sync = require('../lib/commands/sync')
 const inquirer = require('inquirer')
@@ -12,29 +11,36 @@ function random () {
   return Math.random().toString(32).substr(2, 5)
 }
 
-const testDataset = [
-  {id: 1, test: r.ISO8601("2017-01-01T00:00:00+00:00")},
-  {id: 2, test: r.ISO8601("2017-01-01T00:00:00+03:00")},
-  {id: 3, test: r.ISO8601("2017-01-01T00:00:00-07:00")},
-  {id: r.ISO8601("2017-01-01T00:00:00+00:00"), test: "UTC"},
-  {id: r.ISO8601("2017-01-01T00:00:00-07:00"), test: "UTC-7"},
-  {id: r.ISO8601("2017-01-01T00:00:00+03:00"), test: "UTC+3"}
-]
-
 describe('commands', function () {
+  const srcHost = 'localhost:28015'
+  const dstHost = 'localhost:28016'
+  const sr = require('rethinkdbdash')({port: 28015, silent: true})
+  const tr = require('rethinkdbdash')({port: 28016, silent: true})
   this.timeout(60000)
+
+  const testDataset = [
+    {id: 1, test: sr.ISO8601('2017-01-01T00:00:00+00:00')},
+    {id: 2, test: sr.ISO8601('2017-01-01T00:00:00+03:00')},
+    {id: 3, test: sr.ISO8601('2017-01-01T00:00:00-07:00')},
+    {id: sr.ISO8601('2017-01-01T00:00:00+00:00'), test: 'UTC'},
+    {id: sr.ISO8601('2017-01-01T00:00:00-07:00'), test: 'UTC-7'},
+    {id: sr.ISO8601('2017-01-01T00:00:00+03:00'), test: 'UTC+3'}
+  ]
 
   describe('clone', function () {
     const testSrcDB = 'test_thinker_' + random()
     const testDstDB = 'test_thinker_' + random()
-    const testTable = 'test1'
+    const options = {
+      sh: srcHost,
+      th: dstHost,
+      sourceDB: testSrcDB,
+      targetDB: testDstDB,
+      noProgress: true
+    }
 
     before(function * () {
-      yield r.connect()
-      yield r.dbCreate(testSrcDB).run()
-      yield r.db(testSrcDB).tableCreate(testTable).run()
-      yield r.db(testSrcDB).table(testTable).insert(testDataset)
-      yield r.dbCreate(testDstDB).run()
+      yield sr.dbCreate(testSrcDB).run()
+      yield tr.dbCreate(testDstDB).run()
     })
 
     beforeEach(function () {
@@ -44,32 +50,43 @@ describe('commands', function () {
     })
 
     it(`should correctly clone test table`, function * () {
+      const testTable = 'test_' + random()
+
+      yield sr.db(testSrcDB).tableCreate(testTable).run()
+      yield sr.db(testSrcDB).table(testTable).insert(testDataset).run()
+
       let logStub = this.sinon.stub(console, 'log')
-      yield * clone({sourceDB: testSrcDB, targetDB: testDstDB})
+      yield * clone(options)
       logStub.restore()
 
-      const srcData = yield r.db(testSrcDB).table(testTable)
+      const srcData = yield sr.db(testSrcDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-      const dstData = yield r.db(testDstDB).table(testTable)
+      const dstData = yield tr.db(testDstDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-
       assert.deepEqual(srcData, dstData)
+      assert.strictEqual(srcData.length, testDataset.length)
     })
 
     after(function * () {
-      yield r.dbDrop(testDstDB).run()
-      yield r.dbDrop(testSrcDB).run()
+      yield tr.dbDrop(testDstDB).run()
+      yield sr.dbDrop(testSrcDB).run()
     })
   })
 
   describe('sync', function () {
     const testSrcDB = 'test_thinker_' + random()
     const testDstDB = 'test_thinker_' + random()
+    const options = {
+      sh: srcHost,
+      th: dstHost,
+      sourceDB: testSrcDB,
+      targetDB: testDstDB,
+      noProgress: true
+    }
 
     before(function * () {
-      yield r.connect()
-      yield r.dbCreate(testSrcDB).run()
-      yield r.dbCreate(testDstDB).run()
+      yield sr.dbCreate(testSrcDB).run()
+      yield tr.dbCreate(testDstDB).run()
     })
 
     beforeEach(function () {
@@ -79,54 +96,53 @@ describe('commands', function () {
     })
 
     it(`should correctly sync test table`, function * () {
-      const testTable = 'test1'
+      const testTable = 'test_' + random()
 
-      yield r.db(testSrcDB).tableCreate(testTable).run()
-      yield r.db(testSrcDB).table(testTable).insert(testDataset).run()
+      yield sr.db(testSrcDB).tableCreate(testTable).run()
+      yield sr.db(testSrcDB).table(testTable).insert(testDataset).run()
 
       let logStub = this.sinon.stub(console, 'log')
-      yield * sync({sourceDB: testSrcDB, targetDB: testDstDB, pickTables: [testTable], noProgress: true})
+      yield * sync(Object.assign(options, {pickTables: [testTable]}))
       logStub.restore()
 
-      const srcData = yield r.db(testSrcDB).table(testTable)
+      const srcData = yield sr.db(testSrcDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-      const dstData = yield r.db(testDstDB).table(testTable)
+      const dstData = yield tr.db(testDstDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-
       assert.deepEqual(srcData, dstData)
       assert.strictEqual(srcData.length, testDataset.length)
     })
 
     it(`should delete any extra records`, function * () {
-      const testTable = 'test2'
+      const testTable = 'test_' + random()
 
-      yield r.db(testSrcDB).tableCreate(testTable).run()
-      yield r.db(testSrcDB).table(testTable).delete().run()
-      yield r.db(testDstDB).tableCreate(testTable).run()
-      yield r.db(testDstDB).table(testTable).insert(testDataset).run()
+      yield sr.db(testSrcDB).tableCreate(testTable).run()
+      yield sr.db(testSrcDB).table(testTable).delete().run()
+      yield tr.db(testDstDB).tableCreate(testTable).run()
+      yield tr.db(testDstDB).table(testTable).insert(testDataset).run()
 
-      const srcDataPre = yield r.db(testSrcDB).table(testTable)
+      const srcDataPre = yield sr.db(testSrcDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-      const dstDataPre = yield r.db(testDstDB).table(testTable)
+      const dstDataPre = yield tr.db(testDstDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
       assert.strictEqual(srcDataPre.length, 0)
       assert.notEqual(dstDataPre.length, 0)
 
       let logStub = this.sinon.stub(console, 'log')
-      yield * sync({sourceDB: testSrcDB, targetDB: testDstDB, pickTables: [testTable], noProgress: true})
+      yield * sync(Object.assign(options, {pickTables: [testTable]}))
       logStub.restore()
 
-      const srcData = yield r.db(testSrcDB).table(testTable)
+      const srcData = yield sr.db(testSrcDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
-      const dstData = yield r.db(testDstDB).table(testTable)
+      const dstData = yield tr.db(testDstDB).table(testTable)
         .orderBy({index: 'id'}).run({timeFormat: 'raw'})
       assert.strictEqual(srcData.length, 0)
       assert.strictEqual(dstData.length, 0)
     })
 
     after(function * () {
-      yield r.dbDrop(testDstDB).run()
-      yield r.dbDrop(testSrcDB).run()
+      yield tr.dbDrop(testDstDB).run()
+      yield sr.dbDrop(testSrcDB).run()
     })
   })
 })
